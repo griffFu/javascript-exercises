0a0a89608870186ae8cd3b024b697e0f3deb35ba
griffFu <gfulto.9@gmail.com>
2022-05-27 15:17:36 -0500

    working on the 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(firstval,lastval) {
+    len = lastval - firstval;
+    let sum = 0;
+    for (let i = 0; i<len.length;i++){
+        let addvalue = firstval + i
+        let newsum =+ addvalue
+    }
+    
 
 };
 
